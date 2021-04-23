62b205ffe09bc71f3ee68ba69bd96a106d945c72
ArvinRad <radanarvin@gmail.com>
2021-04-23 05:59:36 +0000

    Add Tweet Container with hovering and timeago

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,4 @@
-// Count the characters
+// Count the characters and show its the remaining
 
 $(document).ready(function() {
   console.log("Document is ready.");
@@ -8,8 +8,15 @@ $(document).ready(function() {
       if (event.key === "Backspace") count--;
       else count++;
       $("#counter").html(140 - count)
-      if (count > 140) $('#counter').css('color', 'red');
+      if (count > 140) $('#counter').css('color', 'red')
+      else $('#counter').css('color', '#545149');
     }
-  return count;
   });
+  // const list = document.querySelectorAll("div.tweettime");
+  // console.log(list);
+  // for (item of list) {
+  //   const date = $(`${item}`).html();
+  //   const result = $(`${item}`).html($.timeago(new Date(date)));
+  //   console.log(item, date, result)
+  // }
 });
